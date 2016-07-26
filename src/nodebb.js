0870e86ce4706b8f56e52ba6e95d003b21820874
liwei <liwei@tianshemedia.com>
2016-07-26 18:47:20 +0800

    1.增加健壮性 2.注释掉日志输出

---
+++ src/nodebb.js
@@ -0,0 +1,9 @@
+(function (Module, NodeBB) {
+	'use strict';
+
+	Module.exports = {
+		underscore: NodeBB.require('underscore'),
+		winston   : NodeBB.require('winston')
+	};
+
+})(module, require.main);
